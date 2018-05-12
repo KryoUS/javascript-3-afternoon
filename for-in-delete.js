ec2521fff4bee9fa57afad53e196025ee33e3417
unknown <TaurusSHO89@gmail.com>
2018-05-12 13:21:21 -0600

    finished for-in-delete

--- for-in-delete.js
+++ for-in-delete.js
@@ -135,7 +135,14 @@ var secrets = obj => {
   Delete the property password and return the object.
 */
 
-//Code Here
+var removePassword = obj => {
+  for (key in obj) {
+    if (key == "password") {
+      delete obj[key];
+    }
+  }
+  return obj;
+}
 
 
 
@@ -154,8 +161,11 @@ var deleteTheBigNumbers = {
   Write a for in loop that deletes every property from the object deleteTheBigNumbers whose value is greater than 100.
 */
 
-//Code Here
-
+for (key in deleteTheBigNumbers) {
+  if (deleteTheBigNumbers[key] > 100) {
+    delete deleteTheBigNumbers[key];
+  }
+}
 
 
 ////////// PROBLEM 7 //////////
@@ -167,7 +177,14 @@ var deleteTheBigNumbers = {
   Return the updated object.
 */
 
-//Code Here
+var startsWithK = obj => {
+  for (key in obj) {
+    if (key.startsWith("k")) {
+      delete obj[key];
+    }
+  }
+  return obj;
+}
 
 
 
@@ -182,6 +199,13 @@ var deleteTheBigNumbers = {
   (hint: the method includes() may be of use...)
 */
 
-//Code Here
+var hiddenTreasure = obj => {
+  for (key in obj) {
+    if (obj[key].indexOf("treasure") == -1) {
+      delete obj[key];
+    }
+  }
+  return obj;
+}
 
 
